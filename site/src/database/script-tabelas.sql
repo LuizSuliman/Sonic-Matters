9d2f3b0c469c5a3ebf51cc53c4394072e5906044
Luíz Suliman <luiz.suliman@sptech.school>
2023-05-16 23:47:47 -0300

    Cadastro e Login funcionando! + Script do Banco de Dados

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,52 +1,37 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
 /*
 comandos para mysql - banco local - ambiente de desenvolvimento
 */
 
-CREATE DATABASE aquatech;
-
-USE aquatech;
+CREATE DATABASE sonicMatters;
+USE sonicMatters;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    email VARCHAR(45),
+    senha VARCHAR(45),
+    personagem VARCHAR(25)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
+CREATE TABLE votacao (
+	idVotacao INT PRIMARY KEY AUTO_INCREMENT,
+    tipo VARCHAR(45)
+) AUTO_INCREMENT = 10;
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
+CREATE TABLE voto (
+	idVoto INT PRIMARY KEY AUTO_INCREMENT,
+    escolha VARCHAR(45),
+    fkUsuario INT,
+	CONSTRAINT fkVotoUsuario FOREIGN KEY (fkUsuario)
+		REFERENCES usuario(idUsuario),
+	fkVotacao INT,
+	CONSTRAINT fkVotoVotacao FOREIGN KEY (fkVotacao)
+		REFERENCES votacao(idVotacao)
+) AUTO_INCREMENT = 100;
 
 
 /*
+DA API ORIGINAL
 comando para sql server - banco remoto - ambiente de produção
 */
 

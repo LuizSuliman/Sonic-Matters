e39630d1b2f34f724c8b367f878d2ec1f3fd78f6
Luíz Suliman <luiz.suliman@sptech.school>
2023-05-21 17:16:59 -0300

    Portal + Navbar de usuário logado

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -25,7 +25,7 @@ function cadastrar(nome, email, senha, personagem) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (nome, email, senha, personagemFavorito) VALUES ('${nome}', '${email}', '${senha}', '${personagem}');
+        INSERT INTO usuario (nome, email, senha, personagem) VALUES ('${nome}', '${email}', '${senha}', '${personagem}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
